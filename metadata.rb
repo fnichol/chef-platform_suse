3459668c68666790b647906b0d284de730240d9f
Fletcher Nichol <fnichol@nichol.ca>
2011-10-06 15:52:30 -0600

    Update metadata.rb.

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
-maintainer       "Example Com"
-maintainer_email "ops@example.com"
+maintainer       "Fletcher Nichol"
+maintainer_email "fnichol@nichol.ca"
 license          "Apache 2.0"
-description      "Installs/Configures platform_suse"
+description      "Adds patches for Chef running on SuSE platforms."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1"
+version          "0.1.1"
